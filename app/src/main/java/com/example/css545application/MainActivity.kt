629a82be1163ae15bffeeb77386965e40d42216b
Sarah Martel <sarahmmartel@gmail.com>
2024-04-06 20:19:38 -0700

    2 screen app with a button to switch between them

--- app/src/main/java/com/example/css545application/MainActivity.kt
+++ app/src/main/java/com/example/css545application/MainActivity.kt
@@ -6,10 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.css545application.ui.JokeText
 import com.example.css545application.ui.theme.CSS545ApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +23,20 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    JokeApp()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun JokePreview() {
     CSS545ApplicationTheme {
-        Greeting("Android")
+        JokeApp()
     }
 }
+
